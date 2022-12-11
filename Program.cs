8875a27f160f4600277027ee77fc9569a293f66a
moderngamer675 <shahzadhusnain787@Gmail.com>
2022-12-11 16:52:15 +0000

    Middle Square Methods Version 1.0.0.0

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace EPQ_Testing
     {
         static void Main(string[] args)
         {
-            NumberGenerator number = new NumberGenerator(1000, 10000);
+            NumberGenerator number = new NumberGenerator(30, 10000);  //(number of passess to make, # of numbers to generate in each pass)
 
             number.runAlgorithm();
 
@@ -19,11 +19,12 @@ namespace EPQ_Testing
     class NumberGenerator
     {
         private double m = 3;
-        private double tempNumber = 0;
-        int numberOfPasses = 0;
-        int numOfNumbers = 0;
-        int count = 0;
-        Stack stack = new Stack();
+        private double seed  = 0;
+        private int numberOfPasses = 0;
+        private int numOfNumbers = 0;
+        private int count = 0;
+        private Stack stack = new Stack();
+        private Random rnd = new Random();
 
         public NumberGenerator(int p, int n)
         {
@@ -80,16 +81,63 @@ namespace EPQ_Testing
         }
         public void createNumbers()
         {
+            string number;
+            double seed = 0;
+            double num1 = 0;
+            double num2 = 0;
+
+            Stack stack = new Stack();
+            Random rnd = new Random();
+
+            do
+            {
+                seed = DateTime.Now.Ticks % 1000;
+            }
+            while (seed < 213 && seed / 2 != 0);
+
+            stack.Push(seed);
+            num1 = Math.Pow(seed, 2);
+
             for (int i = 0; i < numOfNumbers; i++)
             {
-                tempNumber = DateTime.Now.Ticks;
+                number = Convert.ToString(num1);
 
-                if (tempNumber % m <= 3)
+                if (number.Length == 6)
                 {
-                    tempNumber = tempNumber % m;
-                    tempNumber++;
-                    stack.Push(tempNumber);
+                    double chance = rnd.NextDouble();
+
+                    if (chance < 0.5)
+                    {
+                        number = number.Substring(0, 4);
+                        number = number.Substring(1, 3);
+                    }
+                    else
+                    {
+                        number = number.Substring(1, 4);
+                        number = number.Substring(1, 3);
+                    }
+
+                    num2 = Convert.ToDouble(number);
+                    stack.Push(num2);
+
+                    num1 = Math.Pow(num2, 2);
                 }
+
+                else if (number.Length == 5)
+                {
+                    number = number.Substring(0, 4);
+                    number = number.Substring(1, 3);
+
+                    num2 = Convert.ToDouble(number);
+
+                    stack.Push(num2);
+                    num1 = Math.Pow(num2, 2);
+                }
+            }
+
+            for (int i = 0; i < stack.Count; i++)
+            {
+                Console.WriteLine(stack.Pop());
             }
         }
 
